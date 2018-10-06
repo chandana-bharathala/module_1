611d5d56cb22f85c49ea11da0ff3c3e82578e957
chandana-bharathala <bharathalachandana@gmail.com>
2018-10-06 12:26:50 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- module3/3-sum/Solution.java
+++ module3/3-sum/Solution.java
@@ -1,49 +1,17 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
-class Solution
-{
-public static void main(String[] args)
-{
-	int []a;
-	int n;
-   // System.out.println("enter array size");
-    Scanner sc= new Scanner(System.in);
-    n=sc.nextInt();
-    a=new int[n];
-    //System.out.println("enter array elements");
-    for(int i=0;i<a.length;i++)
-   {
-       a[i]=sc.nextInt();
-   }
-   Arrays.sort(a);
-for(int i=0;i<a.length;i++)
-{
-System.out.print(a[i]);
-}
-for(int i=0;i<a.length;i++)
-{
- int j=i+1;
- int k=a.length-1;
-while(j<k)
-{
-if((a[i]+a[j]+a[k])==0)
-{
- System.out.println("true");
-  return;
-}
-else if((a[i]+a[j]+a[k])<0)
-{
-j++;
-}
-else if((a[i]+a[j]+a[k])>0)
-{
-k--;
-}
-
-}
-
-}
-System.out.println("false");
+public class Solution {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	//System.out.println("enter the size");
+	int n=sc.nextInt();
+	int[] arr=new int[n];
+	//System.out.println("enter the elements");
+	for(int i=0;i<n;i++)
+	{
+		arr[i]=sc.nextInt();
+	}
+	Sum3 s=new Sum3();
+	s.sum(arr,n);	
 }
 }
