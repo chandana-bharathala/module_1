fef4b0ad42af142a4f4d5ad8d5d49a9d26ebf8a5
chandana-bharathala <bharathalachandana@gmail.com>
2018-10-25 23:00:51 +0530

    18031j0023

--- module 9/Merge sort/9.1 Merge Sort Improvements/Solution.java
+++ module 9/Merge sort/9.1 Merge Sort Improvements/Solution.java
@@ -1,103 +1,95 @@
 import java.util.Scanner;
 
 public class Solution {
-	void merge(int arr[], int l, int m, int r) 
-    {  
- 
-    	int n1 = m-l+1; 
-    	int n2 = r - m; 
-        int L[] = new int [n1]; 
-        int R[] = new int [n2]; 
-        for (int i=0; i<n1; ++i)
-        {
-            L[i] = arr[l + i];
-        }
-        for (int j=0; j<n2; ++j) 
-        {
-            R[j] = arr[m + 1+ j]; 
-        }
-        int i = 0, j = 0; 
-        int k = l; 
-        while (i < n1 && j < n2) 
-        { 
-            if (L[i] <= R[j]) 
-            { 
-                arr[k] = L[i]; 
-                i++; 
-            } 
-            else
-            { 
-                arr[k] = R[j]; 
-                j++; 
-            } 
-            k++; 
-        } 
-        while (i < n1) 
-        { 
-            arr[k] = L[i]; 
-            i++; 
-            k++; 
-        } 
-        while (j < n2) 
-        { 
-            arr[k] = R[j]; 
-            j++; 
-            k++; 
-        } 
-    }
-	void insertion(int arr[], int l, int r)
-	{
-		
-		int n=arr.length;
-		for(int i=1;i<n;i++)
-		{
-			int min=arr[i];
-			int j=i-1;
-			while(j>0&&arr[j]>min)
-			{
-				arr[j+1]=arr[j];
-				j--;
-			}
-			arr[j+1]=min;
-		}
-    	
-	}
-	 void sort(int arr[], int l, int r) 
+	 void merge(String arr[], int l, int m, int r) 
+	    {  
+	        
+	    	
+	    	int n1 = m-l+1; 
+	    	int n2 = r - m; 
+	        String L[] = new String [n1]; 
+	        String R[] = new String [n2]; 
+	        for (int i=0; i<n1; ++i)
+	        {
+	            L[i] = arr[l + i];
+	        }
+	        for (int j=0; j<n2; ++j) 
+	        {
+	            R[j] = arr[m + 1+ j]; 
+	        }
+	        int i = 0, j = 0; 
+	        int k = l; 
+	        while (i < n1 && j < n2) 
+	        { 
+	        	int s=L[i].compareTo(R[j]);
+	            if (s<0) 
+	            { 
+	                arr[k] = L[i]; 
+	                i++; 
+	            } 
+	            else
+	            { 
+	                arr[k] = R[j]; 
+	                j++; 
+	            } 
+	            k++; 
+	        } 
+	        while (i < n1) 
+	        { 
+	            arr[k] = L[i]; 
+	            i++; 
+	            k++; 
+	        } 
+	        while (j < n2) 
+	        { 
+	            arr[k] = R[j]; 
+	            j++; 
+	            k++; 
+	        } 
+	    } 
+	    void sort(String arr[], int l, int r) 
 	    { 
 	        if(r-l<=7) {
 	        	System.out.println("Insertion sort method invoked...");
-	        	insertion(arr, l,r);
-	        }
-	        else if (l < r) 
+	        	for (int i = l+1; i <=r; i++) {
+					for (int j = i; j > 0; j--) {
+						int s=arr[j-1].compareTo(arr[j]);
+						if(s>0){
+							String temp=arr[j];
+							arr[j]=arr[j-1];
+							arr[j-1]=temp;
+						}
+			
+					}
+				}
+	        } else if (l < r) 
 	        { 
-	        	int m= (l+r)/2; 
+	            int m = (l+r)/2; 
 	            sort(arr, l, m); 
-	            sort(arr , m+1, r);
+	            sort(arr , m+1, r); 
 	            merge(arr, l, m, r); 
-	        }  
+	        } 
 	    } 
-	 void convert(String arr[]) {
-		 int n=arr.length;
-	    	int[] a=new int[n];
-	    	for(int i=0;i<n;i++)
-	    	{
-	    		a[i]=Integer.parseInt(arr[i]);
-	    	}
-	    	sort(a, 0, a.length-1);
-	    	String s1="["+a[0];
-	        for (int i=1; i<a.length; i++) 
-	        	 s1=s1+","+a[i];
+	    
+	    static void printArray(String st[]) 
+	    { 
+	        String s1="["+st[0];
+	        for (int i=1; i<st.length; i++) 
+	        	 s1=s1+","+st[i];
 	        System.out.println(s1+"]"); 
 	        System.out.println(); 
-	 }
-public static void main(String[] args) {
-	Scanner sc=new Scanner(System.in);
-	Solution m=new Solution();
-	do {
-		String str=sc.next();
-		String[] str1= str.split(",");
-		m.convert(str1);
-	}while(sc.hasNext());
-}
+	    } 
+	    public static void main(String args[]) 
+	    { 
+	    	Scanner scn=new Scanner(System.in);
+	    	do {
+	    		String st = scn.nextLine();
+				String sp[] = st.split(",");
+				Solution m=new Solution();
+	    		m.sort(sp, 0, sp.length-1);   			
+	            printArray(sp); 
+	    	}while(scn.hasNext());
+	    
+	    } 
+	} 
 
-}
